8f93ec92a968c217d9002e6a081ee5245e07ca43
Jacob Koppel <jkoppel93@Jacobs-MacBook-Air.local>
2021-01-17 01:55:32 -0500

    14.142

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeA6GguZx-SLfVrvXGG8d-EJwoqBCn290",
+  authDomain: "expensify-aaec5.firebaseapp.com",
+  databaseURL: "https://expensify-aaec5-default-rtdb.firebaseio.com",
+  projectId: "expensify-aaec5",
+  storageBucket: "expensify-aaec5.appspot.com",
+  messagingSenderId: "412883875658",
+  appId: "1:412883875658:web:7c7fdcfdadfc46178fb3b3",
+  measurementId: "G-KSVXGHWS2B",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase
+  .database()
+  .ref()
+  .set({
+    name: "Jacob Koppel",
+  });
