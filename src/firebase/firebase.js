f0d7e9763dfc51cec521a9c86b72d3312bdf7415
Jacob Koppel <jkoppel93@Jacobs-MacBook-Air.local>
2021-01-17 18:40:53 -0500

    14.148

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -19,33 +19,75 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database
-  .ref()
-  .set({
-    name: "Jacob Koppel",
-    age: 27,
-    stressLevel: 6,
-    job: {
-      title: "Web Developer",
-      company: "Freelance",
-    },
-    location: {
-      city: "Spotswood",
-      country: "United States",
-    },
-  })
-  .then(() => {
-    console.log("Data is saved!");
-  })
-  .catch((e) => {
-    console.log("This failed", e);
-  });
+const onValueChange = database.ref().on(
+  "value",
+  (snapshot) => {
+    const val = snapshot.val();
+    console.log(val.name, "is a", val.job.title, "at", val.job.company);
+  },
+  (e) => {
+    console.log("Error with data fetching", e);
+  }
+);
 
-database.ref().update({
-  stressLevel: 9,
-  "job/company": "Amazon",
-  "location/city": "Seattle",
-});
+setTimeout(() => {
+  database.ref().update({
+    name: "Jake",
+    "job/title": "Developer",
+    "job/company": "Google",
+  });
+}, 3500);
+
+setTimeout(() => {
+  database.ref().off("value", onValueChange);
+}, 7000);
+
+setTimeout(() => {
+  database.ref().update({
+    name: "Jake Koppel",
+    "job/title": "Dev",
+    "job/company": "Facebook",
+  });
+}, 10500);
+
+// database
+//   .ref("location/country")
+//   .once("value")
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log("Error fetching data", e);
+//   });
+
+// database
+//   .ref()
+//   .set({
+//     name: "Jacob Koppel",
+//     age: 27,
+//     stressLevel: 6,
+//     job: {
+//       title: "Web Developer",
+//       company: "Freelance",
+//     },
+//     location: {
+//       city: "Spotswood",
+//       country: "United States",
+//     },
+//   })
+//   .then(() => {
+//     console.log("Data is saved!");
+//   })
+//   .catch((e) => {
+//     console.log("This failed.", e);
+//   });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   "job/company": "Amazon",
+//   "location/city": "Seattle",
+// });
 
 // database
 //   .ref()
