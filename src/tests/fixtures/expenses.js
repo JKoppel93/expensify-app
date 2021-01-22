c279b1e3c3f8363f378c057935d5e3a0b8034674
Jacob Koppel <jkoppel93@Jacobs-MacBook-Air.local>
2021-01-22 04:49:34 -0500

    Get firebase expenses on start (15.158)

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -2,14 +2,14 @@ import moment from "moment";
 
 export default [
   {
-    id: 1,
+    id: "1",
     description: "one",
     note: "note",
     amount: 195,
     createdAt: 0,
   },
   {
-    id: 2,
+    id: "2",
     description: "two",
     note: "note2",
     amount: 109500,
@@ -18,7 +18,7 @@ export default [
       .valueOf(),
   },
   {
-    id: 3,
+    id: "3",
     description: "three",
     note: "note3",
     amount: 4500,
